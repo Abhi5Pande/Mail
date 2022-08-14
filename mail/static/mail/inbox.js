15a8bc43bd620d36f92e29bc598fdb68835d5320
abhijit <abhi.pachpande@gmail.com>
2022-08-14 22:55:47 +0530

    compose,send and inbox implemented

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,93 @@
+console.log("THis is the JS file ")
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  console.log("main function");
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+  console.log("Inside compose emails");
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+  document.querySelector('#compose-form').onsubmit = () => {
+     console.log("Form submit btm clicked");
+    let recipients = document.querySelector('#compose-recipients').value;
+      let subject = document.querySelector('#compose-subject').value;
+      let body = document.querySelector('#compose-body').value;
+     console.log(recipients,subject,body);
+    fetch('/emails',{
+      method:'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body:body 
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+    })
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+  load_mailbox('sent');
+  return false;
+  }
+}
+function show_mail(emails,mailbox){
+console.log("In show mail")
+ 
+    for(let i = 0 ; i < emails.length ; i++)
+    {
+      console.log(emails[i]);
+      let mail = document.createElement("div");
+      mail.classList.add('email');
+      if(emails[i].read)
+        {
+          mail.classList.add('is_read')
+        }
+      if(mailbox==='sent'){
+      mail.innerHTML = `
+        <div class="sent_mailbox"> To: ${emails[i].recipients} </div>
+        <div> Subject - ${emails[i].subject} </div>
+        <div> ${emails[i].timestamp}</div>
+      ` 
+      }
+      else if(mailbox==='inbox'){
+        mail.innerHTML = `
+        <div class="sent_mailbox"> From: ${emails[i].sender} </div>
+        <div> Subject - ${emails[i].subject} </div>
+        <div> ${emails[i].timestamp}</div>
+      `
+      }
+      document.querySelector('#emails-view').append(mail);
+    }
+
+}
+function load_mailbox(mailbox) {
+  console.log("IN load mail section")
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  fetch(`/emails/${mailbox}`)
+  .then( response => response.json())
+  .then(emails => {
+    console.log(emails);
+    show_mail(emails,mailbox)
+  });
+}
